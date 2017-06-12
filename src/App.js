041a6597318b86f09c0dbc79de52a194eb2b3a8c
Maze5656 <JManderley7@yahoo.com>
2017-06-12 10:42:12 -0400

    New favicon and Title

--- src/App.js
+++ src/App.js
@@ -6,13 +6,23 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+          <title>A title</title>
+            <h1>
+                Welcome
+            </h1>
+          <pre className="Navigation">
+              <p>
+                  <a href="#">  Membership  </a>|
+                  <a href="#">  Calendar  </a>|
+                  <a href="#">  About  </a>
+              </p>
+          </pre>
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+          <img src={logo} className="App-logo" alt="logo" />
       </div>
     );
   }
