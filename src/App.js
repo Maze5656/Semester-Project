01c1d284787a1586d4f7bdd4736b27212a9ec484
Maze5656 <JManderley7@yahoo.com>
2017-07-17 15:28:49 -0400

    API added

--- src/App.js
+++ src/App.js
@@ -2,9 +2,54 @@ import React, { Component } from 'react';
 import './App.css';
 import NavBar from './NavBar'
 import logo1 from './logo.png';
+import Request from 'superagent';
+import TemperatureConverter from 'cis137-components';
 
 class App extends Component {
+
+    constructor()
+    {
+        super();
+        this.state = {
+            temper: ""
+        };
+    }
+
+    componentWillMount() {
+
+        }
+
+
+    componentDidMount() {
+        Request
+            .get('api.openweathermap.org/data/2.5/weather?zip=21157,us&APPID=${process.env.REACT_APP_WEATHER_API_KEY}')
+            .end((err, res) => {
+            if(err){
+                console.error(err);
+                return;
+            }
+
+            console.log(res);
+            this.setState({
+                temper: res.body.main.temp
+            });
+        })
+    }
+
+    componentWillReceiveProps(nextProps) {
+
+    }
+
+    componentWillUpdate(nextProps, nextState) {
+
+    }
+
+    componentWillUnmount() {
+
+    }
+
   render() {
+
     return (
         <div className="Wrapper">
           <div className="App">
@@ -19,6 +64,9 @@ class App extends Component {
               <em>Under-Construction</em>
             </p>
           </div>
+            <p>
+                {this.state.temper}
+            </p>
         </div>
     );
   }
